c5bdfa10858fe9b159650044b366f9314066ae32
ixpl0 <kykymapua+github@gmail.com>
2025-12-14 01:43:06 +0400

    feat: add masonry layout for memo cards on large screens

--- app/components/memo/MemoList.vue
+++ app/components/memo/MemoList.vue
@@ -1,18 +1,22 @@
 <template>
-  <div
-    class="space-y-3"
-    data-testid="memo-list"
-  >
-    <MemoCard
-      v-for="item in items"
-      :key="item.id"
-      :memo="item"
-    />
+  <div data-testid="memo-list">
     <div
       v-if="items.length === 0"
       class="text-center py-8 text-base-content/60"
     >
       {{ emptyMessage }}
+    </div>
+    <div
+      v-else
+      class="columns-1 md:columns-2 xl:columns-3 gap-4"
+    >
+      <div
+        v-for="item in items"
+        :key="item.id"
+        class="break-inside-avoid mb-4"
+      >
+        <MemoCard :memo="item" />
+      </div>
     </div>
   </div>
 </template>
